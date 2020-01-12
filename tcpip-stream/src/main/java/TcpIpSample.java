e53877647e640487d8a46c0ac7812f4fcf2f402a
Marco Antonio Anastacio Cintra <anastaciocintra@gmail.com>
2020-01-12 16:55:30 -0300

    adding TcpIp Stream sample

--- tcpip-stream/src/main/java/TcpIpSample.java
+++ tcpip-stream/src/main/java/TcpIpSample.java
@@ -1,3 +1,8 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
 import com.github.anastaciocintra.escpos.EscPos;
 import com.github.anastaciocintra.output.TcpIpOutputStream;
 
