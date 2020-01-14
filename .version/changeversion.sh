f89b2a6fe65565e285ac130f74b6de9e152f55c5
Marco Antonio Anastacio Cintra <anastaciocintra@gmail.com>
2020-01-14 17:20:59 -0300

    changing version

--- .version/changeversion.sh
+++ .version/changeversion.sh
@@ -1,5 +1,5 @@
 old="3.0.0"
-new="4.0.0-SNAPSHOT"
+new="4.0.0"
 
 find ../ -type f \( -name "pom.xml" -o -name "build.gradle*" \) \
   -exec sed -i -e "s/<revision>$old<\/revision>/<revision>$new<\/revision>/g; s/escpos-coffee:$old/escpos-coffee:$new/g" {} \;
